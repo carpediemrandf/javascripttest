c7e1a37aa9049b847b99d924dc3bd62dd8a8d1d5
carpediemrandf <66466050+carpediemrandf@users.noreply.github.com>
2020-06-17 14:24:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const ccBt = document.querySelector('.colorBtn');
+const bodyBcg = document.querySelector('body');
+
+const colors = ['yellow', 'red', 'green', '#3b5998']
+
+ccBt.addEventListener('click', changeColor);
+
+function changeColor(){
+ 
+ let random = Math.floor(Math.random()*colors.length)
+ bodyBcg.style.backgroundColor = colors[random];
+}
